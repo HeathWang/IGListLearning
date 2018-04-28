59b648f53245f674c7797d4be7423a67b12228f0
HeathWang <yishu.jay@gmail.com>
2018-04-28 10:49:56 +0800

    project init

---
+++ IGListLearning/IGListLearning/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IGListLearning
+//
+//  Created by Heath on 2018/4/28.
+//  Copyright © 2018 Heath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IGListLearning/IGListLearning/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  IGListLearning
+//
+//  Created by Heath on 2018/4/28.
+//  Copyright © 2018 Heath. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
