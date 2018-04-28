a878480f6ffbd912b4a734744594a0339c621e0e
HeathWang <yishu.jay@gmail.com>
2018-04-28 11:15:57 +0800

    update base ViewController

---
+++ IGListLearning/IGListLearning/ViewControllers/HWBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  HWBaseViewController.h
+//  IGListLearning
+//
+//  Created by Heath on 2018/4/28.
+//  Copyright © 2018 Heath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HWBaseViewController : UIViewController
+
+@end
